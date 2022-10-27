91ac94374320846b2d002cad6a340ed9ce79f03d
mikhael2003 <mikhaelsiman2003@gmail.com>
2022-10-27 09:30:18 +0700

    push 1

--- script.js
+++ script.js
@@ -17,9 +17,9 @@
 let txtNpm = document.getElementById("npm")
 let txtnama = document.getElementById("nama")
 let listMhs = document.getElementById("listmahasiswa")
-
+let tblMhs = document.getElementById("tblMahasiswa")
 let data = []
-
+tampil()
 
 
 
@@ -31,15 +31,36 @@ function simpan(){
 
 console.log(txtNpm.value)
 console.log(txtnama.value)
+
+//cek apakah data di dalam localstorage//
+if (localStorage.getItem("lsMahasiswa")===null){
+//jika localstorage dengan key lsmahasiswa belum ada//
+localStorage.setItem("lsMahasiwa", JSON.stringify(data))
+
 // simpan ke array data
 data.push
 (
-    {"npm" : txtNpm.value,
-     "nama" : txtnama.value}
+    {"npm" : txtNpm.value,"nama" : txtnama.value}
 )
+} 
+else {
+    //jika localstorage dengan key lsmahasiswa sudah ada/sudah disimpan sebelumnya//
+    
+    //ambil dulu data di localstorage dengan key lsmahasiswa//
+    let dataLs = JSON.parse(localStorage.getItem("lsMahasiswa"))
+    console.log(dataLs)
+    //simpan data baru ke dalam localstorage
+    dataLs.push
+(
+    {"npm" : txtNpm.value,"nama" : txtnama.value}
+)
+    //simpan data baru ke dalam localstorage//
+    localStorage.setItem("lsMahasiswa", JSON.stringify(dataLs))
+}
 //panggil function tampil
 tampil()
 }
+
 
 
 function tampil(){
@@ -48,7 +69,15 @@ function tampil(){
     data.forEach(listData)
 }
 
+    //ambil data localstorage dengan key lsMahasiswa
+    let dataTampil = JSON.parse(localStorage.getItem("lsMahasiswa"))
+    dataTampil.forEach(listData)
+
 function listData(item, index) {
-    listMhs.innerHTML = item.npm
-    + "-" + item.npm
+    listMhs.innerHTML += "<li>"+item.npm+ "-" + item.nama + "</li>"
+    
+    tblMhs.innerHTML += `<tr>`
+    tblMhs.innerHTML += `<td>${item.npm}</td>`
+    tblMhs.innerHTML += `<td>${item.nama}</td>`
+    tblMhs.innerHTML += `<tr>`
 }
